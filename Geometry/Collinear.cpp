8977c66bcd1af6706f85b005dd72076f346abd7e
Chet8n <64730540+Chet8n@users.noreply.github.com>
2022-03-10 22:37:00 +0530

    Update Collinear.cpp

--- Geometry/Collinear.cpp
+++ Geometry/Collinear.cpp
@@ -1,5 +1,5 @@
-bool collinear(ll x1, ll y1, ll x2, ll y2, ll x3, ll y3) {
-    ll area = abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+bool collinear(long long x1, long long y1, long long x2, long long y2, long long x3, long long y3) {
+    long long area = abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
     // to get actual area, divide this by 2
     if (area == 0)
         return true;
